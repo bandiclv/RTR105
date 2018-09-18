c1927848b8be5e77c8f775aac529b0908e7c550e
user <renarsbze@hotmail.com>
2018-09-18 15:31:46 +0300

    20180918_15_30

---
+++ dgr_20180918.py
@@ -0,0 +1,24 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> vars()
+{'__builtins__': <module '__builtin__' (built-in)>, '__name__': '__main__', '__doc__': None, '__package__': None}
+>>> a = 10
+>>> a
+10
+>>> a = 'skkrrrt'
+>>> a
+'skkrrrt'
+>>> a = 10
+>>> 
+>>> a
+10
+>>> type(1)
+<type 'int'>
+>>> type(a)
+<type 'int'>
+>>> b = 1.5
+>>> type(b)
+<type 'float'>
+>>> c = 'D'
+>>> 
